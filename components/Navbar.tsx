abbb70f460bf0fc222d3fd1dc7375473428d17f4
Hamza <itshamzajaved@gmail.com>
2024-04-22 12:07:27 +0500

    working on homepage

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,5 @@
+import { SignedIn, UserButton } from '@clerk/nextjs'
+
 import Image from '@/node_modules/next/image'
 import Link from '@/node_modules/next/link'
 import React from 'react'
@@ -7,15 +9,24 @@ const Navbar = () => {
   return (
     <nav className="flex flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
       <Link href="/" className="flex items-center gap-1">
-        <Image src="/icons/logo.svg" alt='Yoom logo' width={32} height={32} 
-        className="max-sm:size-10"/>
-        <p className='text-[26px] font-extrabold text-white max-sm:hidden'>Yoom</p>
+        <Image
+          src="/icons/logo.svg"
+          alt="Yoom logo"
+          width={32}
+          height={32}
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          Yoom
+        </p>
       </Link>
 
-      <div className='flex-between gap-5'>
-        {/* Clerk User Management  */}
+      <div className="flex-between gap-5">
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
 
-        <MobileNav/>
+        <MobileNav />
       </div>
     </nav>
   )
---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+const protectedRoutes = createRouteMatcher([
+  '/',
+  'personal-room',
+  'previous',
+  'recordings',
+  'upcoming',
+  'meeting(.*)',
+])
+
+export default clerkMiddleware((auth, req) => {
+  if (protectedRoutes(req)) auth().protect()
+})
+
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+}
